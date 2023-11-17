3d6045c5ada650d557638ef486b90e7fbdac91ff
Brett Olmstead <scruffyruskin@gmail.com>
2023-11-17 14:27:05 -0500

    Add files via upload

--- AJsStreamlit.py
+++ AJsStreamlit.py
@@ -221,7 +221,6 @@ def getData(key, boardId):
 
 
 df = getData(key = key, boardId="19")
-# st.dataframe(df)
 # st.dataframe(df[["address.streetNumber","address.streetName","Postal FSA"]])
 df["Days Since List"] = 1
 df["association_id"] = df["address.unitNumber"].astype(str) + df["address.streetNumber"].astype(str) + df["address.streetName"].str.upper() + "A1B 2C3"
