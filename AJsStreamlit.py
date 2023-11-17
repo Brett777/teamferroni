7015e51bbafe7e99cec5b64611460888ccacc77a
Brett Olmstead <scruffyruskin@gmail.com>
2023-11-17 14:57:20 -0500

    Add files via upload

--- AJsStreamlit.py
+++ AJsStreamlit.py
@@ -221,12 +221,12 @@ def getData(key, boardId):
 
 
 df = getData(key = key, boardId="19")
-# st.dataframe(df[["address.streetNumber","address.streetName","Postal FSA"]])
+st.dataframe(df[["address.streetNumber","address.streetName","Postal FSA"]])
 df["Days Since List"] = 1
-df["association_id"] = df["address.unitNumber"].astype(str) + df["address.streetNumber"].astype(str) + df["address.streetName"].str.upper() + "A1B 2C3"
+df["association_id"] = df["address.unitNumber"].astype(str) + df["address.streetNumber"].astype(str) + df["address.streetName"].str.upper() + df["address.zip"].astype(str).str.upper()
 df["listDate"] = pd.to_datetime(df["listDate"]).dt.date
 df["soldDate"] = pd.to_datetime(df["soldDate"]).dt.date
-st.write("Historical property data loaded.")
+#st.write("Historical property data loaded.")
 st.dataframe(df)
 with st.spinner("Processing Estimates"):
 
